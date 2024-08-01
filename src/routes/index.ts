24a07e473bdd892636d75a798e698f80ad212cd6
Desmondflexy <ikilodesmond@gmail.com>
2024-08-01 10:24:45 +0100

    add extra wavy

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,7 +4,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-    res.send("👋 Welcome to the chat app");
+    res.send("👋 Welcome to the chat app 👋");
 });
 
 export default router;
